a3682388749888b1c2e46238b617ccd8b52e3f4c
sxboyct <126867965+sxboyct@users.noreply.github.com>
2025-03-26 21:57:52 +0800

    Update guanying.js

--- nodejs/src/spider/video/guanying.js
+++ nodejs/src/spider/video/guanying.js
@@ -3,12 +3,14 @@ import {init, proxy, play, detail as _detail} from '../../util/pan.js';
 import {jsoup} from "../../util/htmlParser.js";
 import axios from "axios";
 import {PC_UA} from "../../util/misc.js";
-import {Yun} from "../../util/yun.js";
 
-const url = 'https://www.gying.org';
-const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)';
+const url = 'https://www.gying.org'
 
-let globalCookies = null;
+const classes = [
+  {type_id: '/mv/------', type_name: '电影'},
+  {type_id: '/tv/------', type_name: '剧集'},
+  {type_id: '/ac/------', type_name: '动漫'}
+]
 
 async function getHtml(config) {
   try {
@@ -16,8 +18,9 @@ async function getHtml(config) {
       url: config.url,
       method: config.method || 'GET',
       headers: config.headers || {
-        'User-Agent': UA,
-        'Cookie': globalCookies ? globalCookies.join('; ') : ''
+        'User-Agent': PC_UA,
+        "cookie": "HMACCOUNT_BFESS=6BF4131F2D346AC8; BAIDUID_BFESS=074D05A61BAC10713B08151BCBB5E7C8:FG=1; ZFY=HMyObm7phQtwOkcgQWY6OBQYsx:ABX8bf0vbEsOUTHUY:C; BDUSS_BFESS=xzbVJzRHFEQ0dpNkExdDg1YTJxOG5hTzdJZUVQelN4TGZWYS14ZkV2c2RXd3RvSVFBQUFBJCQAAAAAAAAAAAEAAABdYPAlbGlmZcix0ru33bCytqgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB3O42cdzuNnRG",
+        "host": "hm.baidu.com",
       },
       data: config.data || '',
       responseType: config.responseType || '',
@@ -27,54 +30,12 @@ async function getHtml(config) {
   }
 }
 
-async function request(reqUrl) {
-  const resp = await req.get(reqUrl, {
-    headers: {
-      'User-Agent': UA,
-      'Cookie': globalCookies ? globalCookies.join('; ') : ''
-    },
-  });
-  return resp.data;
+const pq = (html) => {
+  const jsp = new jsoup();
+  return jsp.pq(html);
 }
-
-async function login() {
-  if (globalCookies) return globalCookies;
-
-  const loginUrl = `${url}/user/login`;
-  const loginData = {
-    siteid: '1',
-    dosubmit: '1',
-    cookietime: '10506240',
-    username: 'sxboyc',
-    password: 'NP8Gpx0jPU1b0j'
-  };
-
-  try {
-    const response = await axios.post(loginUrl, new URLSearchParams(loginData), {
-      headers: {
-        'User-Agent': UA,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      maxRedirects: 0
-    });
-    globalCookies = response.headers['set-cookie'];
-    return globalCookies;
-  } catch (error) {
-    console.error('Login failed:', error);
-    return null;
-  }
-}
-
-const classes = [
-  {type_id: '/mv/------', type_name: '电影'},
-  {type_id: '/tv/------', type_name: '电视剧'},
-  {type_id: '/ac/------', type_name: '动漫'}
-];
-
-const tags = classes.map(item => item.type_name);
 
 async function home(_inReq, _outResp) {
-  await login();
   let filterObj = {};
   return ({
     class: classes,
@@ -83,19 +44,19 @@ async function home(_inReq, _outResp) {
 }
 
 async function category(inReq, _outResp) {
-  await login();
   const tid = inReq.body.id;
   const pg = inReq.body.page;
   let page = pg || 1;
   if (page == 0) page = 1;
-
-  let html = await request(`${url}${tid}${page}`);
-  const $ = new jsoup().pq(html);
-  let videos = [];
-
-  const scriptContent = $('script').filter((_, script) => 
-    script.children[0]?.data.includes('_obj.header')
-  )[0].children[0].data;
+  
+  const fullUrl = `${url}${tid}${page}`
+  const { data } = await getHtml(fullUrl);
+  const $ = pq(data)
+  
+  let videos = []
+  const scriptContent = $('script').filter((_, script) => {
+    return $(script).html().includes('_obj.header');
+  }).html();
 
   const jsonStart = scriptContent.indexOf('{');
   const jsonEnd = scriptContent.lastIndexOf('}') + 1;
@@ -107,17 +68,17 @@ async function category(inReq, _outResp) {
     
     inlistData["i"].forEach((item, index) => {
       videos.push({
-        "vod_name": inlistData["t"][index],
-        "vod_id": item,
-        "vod_pic": `https://s.tutu.pm/img/${inlistData["ty"]}/${item}.webp`,
-        "vod_remarks": inlistData["g"][index]
-      });
-    });
+        vod_name: inlistData["t"][index],
+        vod_id: item,
+        vod_pic: `https://s.tutu.pm/img/${inlistData["ty"]}/${item}.webp`,
+        vod_remarks: inlistData["g"][index],
+      })
+    })
   }
 
   return ({
     page: parseInt(page),
-    pagecount: 1,
+    pagecount: 1, // You might want to extract this dynamically
     limit: videos.length,
     total: videos.length,
     list: videos,
@@ -125,14 +86,14 @@ async function category(inReq, _outResp) {
 }
 
 async function detail(inReq, _outResp) {
-  await login();
-  let html = await request(`${url}/res/downurl${inReq.body.id}`);
-  const respstr = JSON.parse(html);
-
+  const downUrl = `https://www.gyg.la/res/downurl/${inReq.body.id}`
+  const { data } = await getHtml(downUrl);
+  const respstr = JSON.parse(data);
+  
   let vod = {
-    "vod_name": inReq.body.id,
-    "vod_id": inReq.body.id,
-  };
+    vod_name: '', // You'll need to extract this from somewhere
+    vod_id: inReq.body.id,
+  }
 
   if (respstr.hasOwnProperty('panlist')) {
     vod.vod_play_from = '网盘';
@@ -145,26 +106,26 @@ async function detail(inReq, _outResp) {
 }
 
 async function search(inReq, _outResp) {
-  await login();
   const pg = inReq.body.page || 1;
   const wd = inReq.body.wd;
   
-  let html = await request(`${url}/s/1---${pg}/${encodeURIComponent(wd)}`);
-  const $ = new jsoup().pq(html);
-  let videos = [];
-
+  const searchUrl = `${url}/s/1---${pg}/${encodeURIComponent(wd)}`
+  const { data } = await getHtml(searchUrl);
+  const $ = pq(data);
+  
+  let videos = []
   $('.v5d').each((index, element) => {
     const name = $(element).find('b').text().trim() || 'N/A';
     const imgUrl = $(element).find('picture source[data-srcset]').attr('data-srcset') || 'N/A';
     const additionalInfo = $(element).find('p').text().trim() || 'N/A';
-    const pathMatch = $(element).find('a').attr('href') || 'N/A';
+    const pathMatch = $(element).find('a').attr('href') || 'N/A'
 
     videos.push({
-      "vod_name": name,
-      "vod_id": pathMatch,
-      "vod_remarks": additionalInfo,
-      "vod_pic": imgUrl,
-    });
+      vod_name: name,
+      vod_id: pathMatch,
+      vod_pic: imgUrl,
+      vod_remarks: additionalInfo,
+    })
   });
 
   return {
