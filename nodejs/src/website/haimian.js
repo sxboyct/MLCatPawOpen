ed6c30b81d657ee5afdece5c6d1e285d29f102a0
sxboyct <126867965+sxboyct@users.noreply.github.com>
2025-04-01 09:32:52 +0800

    Update haimian.js

--- nodejs/src/website/haimian.js
+++ nodejs/src/website/haimian.js
@@ -1,14 +1,14 @@
 export const getCache = async (server) => {
-  const obj = await server.db.getObjectDefault(`/leijing`, {})
+  const obj = await server.db.getObjectDefault(`/haimian`, {})
   // 优先级：缓存->配置文件->兜底域名
-  return obj?.url || server.config.wogg.url || 'https://leijing.xyz'
+  return obj?.url || server.config.haimian.url || 'https://www.hmxz.org'
 }
 
 export const setCache = async (server, value) => {
-  await server.db.push(`/leijing/url`, value);
+  await server.db.push(`/haimian/url`, value);
 }
 
-export default async function leijing(fastify) {
+export default async function haimian(fastify) {
   fastify.get('/url', async (req, res) => {
     res.send({
       code: 0,
